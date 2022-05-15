5c22d1fe0dcb13c9eea11114fbc36a67ab2dde43
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-05-15 15:04:07 +0530

    Navbar Toggle complete

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -1,8 +1,15 @@
-import React from 'react'
-import { Navbar, NavbarToggler, Nav, NavLink, NavItem, NavbarBrand, Collapse} from "reactstrap"
+import React,{useState,useContext} from 'react'
+import { Navbar, NavbarToggler, Nav, NavLink, NavItem, NavbarBrand, Collapse, NavbarText} from "reactstrap"
 import { Link } from "react-router-dom"
+import {UserContext} from "../Context/UserContext"
+
 
 const Header = () => {
+    const [open, setOpen]=useState(false);
+    const userContext=useContext(UserContext);
+
+    const toggle=()=>setOpen(!open);
+
     return (
         <Navbar color="info" light expand="md">
       <NavbarBrand>
@@ -10,18 +17,24 @@ const Header = () => {
           Git-Firebase App
         </Link>
       </NavbarBrand>
-      <NavbarToggler />
-        <Collapse navbar>
-        <Nav className='ml-auto' navbar>
-          <NavItem>
-            <NavLink className="text-white" to="/signout">Signup</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className="text-white" to="/signin">Signin</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink className="text-white" to="/logout">Logout</NavLink>
-          </NavItem>
+      <NavbarText className='text-white'>{userContext.user?.email? userContext.user.email:""}</NavbarText>
+      <NavbarToggler onClick={toggle}/>
+
+        <Collapse open={open} navbar>
+            
+        <Nav className='ms-auto' navbar>
+            {
+                userContext.user?(<><NavItem>
+                    <NavLink tag={Link} className="text-white" to="/">Logout</NavLink>
+                  </NavItem></>):(<><NavItem>
+                    <NavLink tag={Link} className="text-white" to="/signout">Signup</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink tag={Link} className="text-white" to="/signin">Signin</NavLink>
+                  </NavItem></>)
+            }
+          
+          
         </Nav>
         </Collapse>
     </Navbar>
