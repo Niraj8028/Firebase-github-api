0c3ae57668b55bd3a237842337b1a6232bd71dc8
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-05-16 14:13:10 +0530

    Signin creatted

--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -1,9 +1,95 @@
-import React from 'react'
+import React,{useState,useContext} from 'react'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import {
+  Container,Form,Button,FormGroup,Label,Col,Input,Row,Card,CardBody,CardFooter,CardHeader
+} from "reactstrap"
+import { UserContext } from '../Context/UserContext'
+import {toast} from "react-toastify"
+import {Navigate} from "react-router-dom"
+
 
 const Signin=()=> {
+  const[email,setEmail]=useState("")
+  const[password,setPassword]=useState("")
+  const userContext=useContext(UserContext)
+  
+ 
+
+  const handleSignin=()=>{
+    firebase
+    .auth()
+    .signInWithEmailAndPassword(email,password)
+    .then(res=>{
+      console.log(res)
+      userContext.setUser({email:res.user.email,uid:res.user.uid});
+    })
+    .catch(error=>{
+      console.log(error)
+      toast(error.message,{
+        type:"error"
+      })
+    })
+  }
+
+    const handleFormSubmit=(e)=>{
+      e.preventDefault();
+      handleSignin();
+  
+  }
+  if(userContext.user?.uid){
+    return <Navigate to="/"/>
+  }
   return (
-    <div>Signin</div>
-  )
+		<Container className='text-center'>
+			<Row>
+				<Col lg={6} className='offset-lg-3 mt-5'>
+					<Card>
+						<Form onSubmit={handleFormSubmit}>
+							<CardHeader className=''>SignIn here</CardHeader>
+							<CardBody>
+								<FormGroup row>
+									<Label for='email' sm={3}>
+										Email
+									</Label>
+									<Col sm={9}>
+										<Input
+											type='email'
+											name='email'
+											id='email'
+											placeholder='provide your email'
+											value={email}
+											onChange={e => setEmail(e.target.value)}
+										/>
+									</Col>
+								</FormGroup>
+								<FormGroup row>
+									<Label for='password' sm={3}>
+										Password
+									</Label>
+									<Col sm={9}>
+										<Input
+											type='password'
+											name='password'
+											id='password'
+											placeholder='your password here'
+											value={password}
+											onChange={e => setPassword(e.target.value)}
+										/>
+									</Col>
+								</FormGroup>
+							</CardBody>
+							<CardFooter>
+								<Button type='submit' block color='primary'>
+									Sign In
+								</Button>
+							</CardFooter>
+						</Form>
+					</Card>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
 
 export default Signin
